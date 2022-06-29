c8bf0763e82023db6005d05deb85f0d5a67776a3
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-06-29 17:58:08 -0300

    correção de erros

--- MAPA.C
+++ MAPA.C
@@ -5,9 +5,19 @@ int main(){
 
     char nome, vacina, data;
     int numLote, codigo;
-    double CPF;
+    double cpf;
     
-    
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+    printf("Digite o seu CPF: ");
+    scanf("%f", &cpf);
+    printf("Digte qual a sua vacina: ");
+    scanf("%s", &vacina);
+    printf("Digite a data da aplicacao: ");
+    scanf("%s", &data);
+    printf("Digite o numero do lote: ");
+    scanf("%d", &numLote);
 
+    printf("%s, %s, %s, %d, %f",nome, vacina, data, numLote, cpf);
 
 }
