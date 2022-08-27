c0d669972510685c2968ede19fb48946584e9717
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-08-27 14:11:33 -0300

    começando projeto mapa sub 53

--- 53/MAPA.c
+++ 53/MAPA.c
@@ -1,1 +1,30 @@
-#include 
+#include <stdio.h>
+
+int main(){
+
+    int codigo, acao;
+    char nome[50];
+    char endereco[250];
+    char pedido[50];
+    float valor;
+
+    printf("1 - Cadastro Pedido \n 2 - Consulta Pedido \n 3 - Emetir relatorio \n 4 - Sair \n");
+    printf("Digite qual comando sera executado: ");
+    scanf("%d", &acao);
+    
+   if ( acao == 1)
+   {
+    
+    printf("Informe seu pedido: ");
+    scanf("%c", &pedido);
+    codigo = 1;
+    scanf("%d", &codigo);
+    printf("Seu pedido e: %c\n", pedido);
+    printf("seu codigo e: %d", codigo);
+
+
+   }
+   
+
+
+}
