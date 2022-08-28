12e694356815c8dbe6d84cb71c8a94fd55778e3d
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-08-28 16:01:11 -0300

    .

--- 53/MAPA.c
+++ 53/MAPA.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    int codigo, acao;
+    int codigo = 0, menu = 0;
+    int i;
     char nome[50];
+    char infoPedido[50][3];
     char endereco[250];
     char pedido[50];
-    float valor;
+    float valor = 0;
 
     printf("1 - Cadastro Pedido \n 2 - Consulta Pedido \n 3 - Emetir relatorio \n 4 - Sair \n");
     printf("Digite qual comando sera executado: ");
-    scanf("%d", &acao);
+    scanf("%d", &menu);
     
-   if ( acao == 1)
+   switch (menu)
    {
-    
-    printf("Informe seu pedido: ");
-    scanf("%c", &pedido);
-    codigo = 1;
-    scanf("%d", &codigo);
-    printf("Seu pedido e: %c\n", pedido);
-    printf("seu codigo e: %d", codigo);
+   case 1:
+    for (i = 0; i < 50; i++)
+    {
+        if(codigo + i != 0)
+        {
+            break;
+        }
+        
+    }
+   
+    printf("Informe seu pedido: \n");
+    scanf("%s", &infoPedido[codigo][0]);
+    printf("Informe seu nome: \n");
+    scanf("%s", &infoPedido[codigo][1]);
+    printf("Infome seu endereço: \n");
+    scanf("%s", &infoPedido[codigo][2]);
+    printf("Informe o valor do pedido: \n");
+    scanf("%s", infoPedido[codigo][3]);
 
 
+    break;
+    case 2:
+    printf("Digite seu codigo: \n");
+
+    break;
+    case 3:
+
+    break;
+    case 4:
+    break
+   
+   default:
+    break;
    }
    
 
