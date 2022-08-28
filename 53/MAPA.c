91c46c27b202d92dede7e1217baba8d5a8a740be
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-08-28 17:04:53 -0300

    .

--- 53/MAPA.c
+++ 53/MAPA.c
@@ -4,8 +4,8 @@
 int main(){
 
     int codigo = 0, menu = 0;
-    int i;
-    char nome[50],  infoPedido[50][50], endereco[250][50], pedido[50][5];
+    int i = 0;
+    char nome[50] = {}, pedido[50][50] = {}, endereco[250][50] = {}, pedido[50][5] = {};
     float valor = 0;
 
     printf("1 - Cadastro Pedido \n 2 - Consulta Pedido \n 3 - Emetir relatorio \n 4 - Sair \n");
@@ -15,27 +15,28 @@ int main(){
     switch (menu)
     {
     case 1:
-    for (i = 0; i < 50; i++)
-    {
-        if(codigo[i] != 0)
+
+        for (i = 0; i < 50; i++)
         {
-            break;
+            if(codigo[i] != 0)
+            {
+                break;
+            }
+            else
+            {
+                codigo[i] = i
+            }
+            
         }
-        else
-        {
-            codigo[i] = i
-        }
-        
-    }
-   
-    printf("Informe seu pedido: \n");
-    scanf("%s", &pedido[codigo][0]);
+                             
+    printf("Informe seu pedido: \n"); //cada caracter ocupa um slot na array
+    scanf("%s", &pedido[codigo][]);
     printf("Informe seu nome: \n");
-    scanf("%s", &nome[codigo][1]);
+    scanf("%s", &nome[codigo][]);
     printf("Infome seu endereço: \n");
-    scanf("%s", &endereco[codigo][2]);
+    scanf("%s", &endereco[codigo][]);
     printf("Informe o valor do pedido: \n");
-    scanf("%s", &valor[codigo][3]);
+    scanf("%s", &valor[codigo][]);
 
 
     break;
